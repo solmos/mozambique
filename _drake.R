65f1337a169eb995848cba08f4021cf5895d98a9
solmos <sergio.olmos@isglobal.org>
2020-08-14 14:06:23 +0200

    Added mergeData.R to source and tried sourcing all functions at once

--- _drake.R
+++ _drake.R
@@ -1,9 +1,14 @@
 source("R/packages.R")
+
+## lapply(list.files(here("R"), full.names = TRUE), source)
+
 source("R/readAOD.R")
 source("R/readAirPollution.R")
 source("R/readAirPollutionBackground.R")
 source("R/readFireCounts.R")
 source("R/readTemperature.R")
+source("R/mergeData.R")
 source("R/plan.R")
+
 
 drake_config(plan)

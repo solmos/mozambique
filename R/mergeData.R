e881c7627bb310903547a0ebb34716ebdb1269bd
solmos <sergio.olmos@isglobal.org>
2020-08-18 14:08:30 +0200

    Created function for merging all data sets

--- R/mergeData.R
+++ R/mergeData.R
@@ -1,17 +1,46 @@
 ## Merge all data sets
-mergeData <- function(air.pollution, background, fire, aod, temperature) {
-  df <- air.pollution %>%
-    left_join(background, by = "date") %>%
+mergeData <- function(airpollution, background, fire, aod, temperature) {
+  aod_wide <- aod %>%
+    pivot_wider(names_from = "tile", values_from = starts_with("aod"))
+
+  df <- airpollution %>%
+    left_join(background, by = "date", suffix = c("", "_background")) %>%
     mutate(
       week_day = weekdays(date),
-      month = month(date),
-      season = ifelse(month %in% 5:10, "dry", "wet"),
+      month = as.integer(month(date)),
+      ## Dry season from May to October (included)
+      season = factor(ifelse(month %in% 5:10, "dry", "wet")),
       weekend = ifelse(week_day %in% c("Saturday", "Sunday"), TRUE, FALSE)
     ) %>%
     left_join(fire %>% select(date, n), by = "date") %>%
-    rename(fires = n)
+    rename(fires = n) %>%
+    ## Removing rows with no measurement in background station
+    drop_na() %>%
+    left_join(aod_wide, by = "date") %>%
+    left_join(temperature %>% select(date, temp_max), by = "date") %>%
+    mutate(
+      station = case_when(
+        station == "Ermelo site - Gert Sibande MP" ~ "ermelo",
+        station == "Middelburg-Nkangala" ~ "middelburg",
+        station == "Hendrina - Nkangala MP" ~ "hendrina")
+    ) %>%
+    pivot_wider(
+      names_from = "station",
+      names_prefix = "pm25_",
+      values_from = "pm25_background") %>%
+    select(date, pm25, ec, oc, everything())
 
   df
 }
 
-mergeData()
+
+## loadd(air_pollution, air_pollution_background, fire, aod, temperature)
+## df_merged <- mergeData(air_pollution, air_pollution_background, fire, aod, temperature)
+## glimpse(df_merged)
+## summary(df_merged)
+## m1 <- lm(pm25 ~ pm25_middelburg + season + fires + aod_med_h20 + temp_max, df_merged)
+## summary(m1)
+## m2 <- lm(ec ~ pm25_middelburg + season + fires + aod_med_h20 + temp_max, df_merged)
+## summary(m2)
+## m3 <- lm(oc ~ pm25_middelburg + season + fires + aod_med_h20 + temp_max, df_merged)
+## summary(m3)

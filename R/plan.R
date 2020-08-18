0b26341b7de85f3688e484d74639e121a4cf0ec4
solmos <sergio.olmos@isglobal.org>
2020-08-18 14:10:28 +0200

    Added merged data target and models target

--- R/plan.R
+++ R/plan.R
@@ -4,6 +4,27 @@ plan <- drake_plan(
   fire = readFireCounts(),
   air_pollution_background = readAirPollutionBackground(),
   temperature = readTemperature(),
-  aod = readAOD()
-  ## EDA
+  aod = readAOD(),
+  data_merged = mergeData(
+    airpollution = air_pollution,
+    background = air_pollution_background,
+    fire = fire,
+    aod = aod,
+    temperature = temperature
+  ),
+  ## Models
+  outcomes = c("pm25", "oc", "ec"),
+  predictors = c("season", "fires", "aod_med_h20", "pm25_middelburg"),
+  models = map(
+    outcomes,
+    ~ fitModel(outcome = ., predictors, data_merged)
+  ),
+  ## Reports
+  eda_report = target(
+    command = {
+      rmarkdown::render(knitr_in("reports/initial-models.Rmd"))
+      file_out("reports/initial-models.html")
+    }
+  )
+
 )
